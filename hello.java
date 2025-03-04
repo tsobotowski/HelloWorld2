9df957709380d358322829a2bcfa022120b6c755
Thaddeus Sobotowski <tsobotowski@gmail.com>
2025-03-04 18:56:59 -0500

    Add display free memory

--- hello.java
+++ hello.java
@@ -4,11 +4,14 @@ import java.time.format.DateTimeFormatter;
 public class hello {
     public static void main(String[] args) {
         LocalDate today = LocalDate.now(); //Gets the date
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy"); //
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy");
         String formattedDate = today.format(formatter);
-    	
+        Runtime runtime = Runtime.getRuntime(); //Get runtime info
+        long freeMemory = runtime.freeMemory(); //Get free memory
+        
         System.out.println("Here ye : important announcement");
         System.out.println("The current date is: " + formattedDate);
+        System.out.println("This program has: " + (freeMemory / 1000000) + " bytes of free memory.");
         System.out.println("Hello, World!");
         System.out.println("Hello, Denise!");
         System.out.println("Hello, Ester!");
