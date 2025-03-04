185c644ab9c25fbcb9d53ff25631f678c5e63a95
Thaddeus Sobotowski <tsobotowski@gmail.com>
2025-03-04 11:25:36 -0500

    Added new line

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        System.out.println("Here ye : important announcement");
         System.out.println("Hello, World!");
     }
 }
