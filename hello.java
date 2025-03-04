78ef0be78affaab0b154f689b2f85120c191e8ff
Thaddeus Sobotowski <tsobotowski@gmail.com>
2025-03-04 11:26:59 -0500

    Added goodbye

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Here ye : important announcement");
         System.out.println("Hello, World!");
+        System.out.println("Goodbye!");
     }
 }
