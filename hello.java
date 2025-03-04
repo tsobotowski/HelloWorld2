635e776af3be9e6e2743d6aaa911985920a31712
Thaddeus Sobotowski <tsobotowski@gmail.com>
2025-03-04 18:46:44 -0500

    Add display date, Closes #1

--- hello.java
+++ hello.java
@@ -1,6 +1,14 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class hello {
     public static void main(String[] args) {
+        LocalDate today = LocalDate.now(); //Gets the date
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy"); //
+        String formattedDate = today.format(formatter);
+    	
         System.out.println("Here ye : important announcement");
+        System.out.println("The current date is: " + formattedDate);
         System.out.println("Hello, World!");
         System.out.println("Hello, Denise!");
         System.out.println("Hello, Ester!");
