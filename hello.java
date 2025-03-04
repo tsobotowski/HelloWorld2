b3540205d6603bcccd16be7ba9fcc8273ada81d1
Thaddeus Sobotowski <tsobotowski@gmail.com>
2025-03-04 17:49:42 -0500

    Add Ester greeting

--- hello.java
+++ hello.java
@@ -3,6 +3,7 @@ public class HelloWorld {
         System.out.println("Here ye : important announcement");
         System.out.println("Hello, World!");
         System.out.println("Hello, Denise!");
+        System.out.println("Hello, Ester!");
         System.out.println("And hello, Charlie!");
         System.out.println("Goodbye!");
     }
