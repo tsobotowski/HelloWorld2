6064cf4e2cae9ba91b01237a290c06e28021a2cf
Thaddeus Sobotowski <tsobotowski@gmail.com>
2025-03-04 18:40:23 -0500

    Fixed class name

--- hello.java
+++ hello.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+public class hello {
     public static void main(String[] args) {
         System.out.println("Here ye : important announcement");
         System.out.println("Hello, World!");
